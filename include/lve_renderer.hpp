8375387f878693c590dbb86211abeeba63605fae
LanduLemon <1147578092@qq.com>
2025-01-08 11:17:23 +0800

    fix(zhc): set isFrameStarted default value

--- include/lve_renderer.hpp
+++ include/lve_renderer.hpp
@@ -46,8 +46,8 @@ class LveRenderer {
   std::unique_ptr<LveSwapChain> lveSwapChain;
   std::vector<VkCommandBuffer> commandBuffers;
 
-  uint32_t currentImageIndex;
-  int currentFrameIndex;
-  bool isFrameStarted;
+  uint32_t currentImageIndex{0};
+  int currentFrameIndex{0};
+  bool isFrameStarted{false};
 };
 }  // namespace lve
